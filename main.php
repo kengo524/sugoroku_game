6848b26d79fb28ea9a8d1fc45adb706eb4fff5f0
kengo524 <kengo.konishi@tokyo.supersoftware.co.jp>
2022-04-21 19:57:39 +0900

    最低限の実装まで

---
+++ main.php
@@ -0,0 +1,30 @@
+<?php
+
+    require_once "./class/player.php";
+    require_once "./class/dice.php";
+    require_once "./class/board.php";
+    require_once "./class/game.php";
+
+    //gameの準備
+    $game = new Game();
+
+    //boardの準備
+    $game->setBoard(new Board("data/board.csv"));
+
+    //playerの準備
+    $game->addPlayer(new Player("Taro"));
+    $game->addPlayer(new Player("Jiro"));
+
+    //diceの準備
+    $game->setDice(new Dice());
+   
+
+    // print_r($game->setBoard(new Board("data/board.csv")));
+    // sleep(10);
+    //gamestart
+    $game->start();
+
+
+
+
+?>
