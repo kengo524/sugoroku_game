3ed37059a7bb0735eb274bd7257068b0ad146084
kengo524 <kengo.konishi@tokyo.supersoftware.co.jp>
2022-04-22 16:09:56 +0900

    dice_numをdice_keyにrename

--- class/Dice.php
+++ class/Dice.php
@@ -2,15 +2,13 @@
     class Dice{
         private $dice_list;
     
-        public function __construct(
-            
-        ){
+        public function __construct(){
             $this->dice_list = array(1,2,3,4,5,6);
         }
         //サイコロの値取得
         public function diceRoll(){
-            $dice_num = array_rand($this->dice_list); 
-            return $this->dice_list[$dice_num];  
+            $dice_key = array_rand($this->dice_list); 
+            return $this->dice_list[$dice_key];  
         }
     }
 
