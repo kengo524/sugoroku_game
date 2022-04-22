85056f14b9d152a6e5d9af2784874340f952faa8
kengo524 <kengo.konishi@tokyo.supersoftware.co.jp>
2022-04-22 16:15:46 +0900

    getPositionとrePositionの処理分別

--- class/Player.php
+++ class/Player.php
@@ -4,9 +4,7 @@
     private int $position;
     //サイコロを持っている数を変数に後々いれてもよいかも。
 
-    public function __construct(
-        $name, 
-    ){
+    public function __construct($name){
         $this->name = $name;
         $this->position = 0;
     }
@@ -15,20 +13,17 @@
     public function getName(){
         return $this->name;
     }
-
     public function getPosition(){
-        return $this->position;
+        return $this->position; 
     }
-    
-    public function getRePosition($number){
+    public function rePosition($number){
         $this->position += $number;
-        return $this->position;
+        if($this->position < 0){//マイナスマス効果によるスタート地点を超えて戻ることの防止
+            $this->position = 0;
+        }
     }
-
   }
-
 //   $tom = new Player("Tom");
-
 //   print($tom->getName());
 //   print($tom->getPosition());
 ?>
